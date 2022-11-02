6bee78dfaac4f0415c65caa0dc522e650d0bcc5f
AccountantAS <117317301+AccountantAS@users.noreply.github.com>
2022-11-03 01:16:00 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+
+<?php 
+ $p = 'Приветствую Вас на моей страничке!';
+?>
+
+<?php 
+ $name = 'Александр';
+ $surname = 'Щедрин';
+ $city = 'Самара';
+ $age = 23;
+ global $name;
+ global $surname;
+ global $city;
+ global $age;
+?>
+
+
+<?php
+include 'main.php';
+?>
+
